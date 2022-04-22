1ac69c8f256195b7b7172a344824b4f212094c0e
Daisy VGS <96205133+daisyvgs@users.noreply.github.com>
2022-04-22 20:42:17 -0300

    Add files via upload

---
+++ Daisy_Flower/script.js
@@ -0,0 +1,3 @@
+let visitante = prompt("Olá! Seja bem-vinda(o). Qual é o seu nome?");
+text = `Olá ${visitante}! Esta é uma página de testes.`;
+document.getElementById("visitante").innerHTML = text;
